d6bc130c7173c9fea129ea430b78d537ebc29c43
Mauricio Pereira <mauricio.pereira@bolivia.assuresoft.com>
2022-01-23 00:09:44 -0400

    max sub string palindrome

---
+++ max-substring-palindrome.js
@@ -0,0 +1,44 @@
+
+function max_palindrome2(str) {
+	const n = str.length;
+	let max = 0;
+
+	let leftIndex = 0;
+	let leftPos;
+	let rightPos;
+	for (let i = 0; i < n; i++) {
+		leftPos = i;
+		rightPos = i + 1;
+		while (leftPos >= 0 && rightPos <= n && str[leftPos] === str[rightPos]) {
+			leftPos --;
+			rightPos ++;
+		}
+
+		leftPos ++;
+		rightPos --;
+
+		if ((rightPos - leftPos) > max) {
+			max = rightPos - leftPos;
+			leftIndex = leftPos;
+		}
+
+		leftPos = i - 1;
+		rightPos = i + 1;
+		while (leftPos >= 0 && rightPos <= n && str[leftPos] === str[rightPos]) {
+			leftPos --;
+			rightPos ++;
+		}
+
+		leftPos ++;
+		rightPos --;
+
+
+		if ((rightPos - leftPos) > max) {
+			max = rightPos - leftPos;
+			leftIndex = leftPos;
+		}
+
+	}
+
+	return str.slice(leftIndex, leftIndex + max + 1);
+}
