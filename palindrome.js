2ca4eef11e8db243363bde3e95ae103fa5ed64c6
Mauricio Pereira <mauricio.pereira@bolivia.assuresoft.com>
2022-01-22 22:39:49 -0400

    add new method palindrome

--- palindrome.js
+++ palindrome.js
@@ -5,3 +5,13 @@ function is_palindrome_every(str) {
 function is_palindrome_reverse(str) {
 	return str === str.split('').reduce((a, char) => char + a, '');
 }
+
+
+function is_palindrome_for(str) {
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] !== str[str.length - i - 1]) return false;
+	}
+
+	return true;
+}
+
