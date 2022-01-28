fca4977bd096baa49475ec7fa8fb1f477fea15d2
Mauricio Pereira <mauricio.pereira@bolivia.assuresoft.com>
2022-01-27 23:19:13 -0400

    queue

---
+++ queue.js
@@ -0,0 +1,35 @@
+function weave(sourceOne, sourceTwo) {
+
+  const qr = new Queue()
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      qr.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek()) {
+      qr.add(sourceTwo.remove());
+    }
+  }
+
+
+  return qr;
+}
+
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
